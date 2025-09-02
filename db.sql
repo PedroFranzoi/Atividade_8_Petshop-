94bcd922217244a2c37138a5c2c7d7be2fc724d3
PedroFranzoi <pedro_franzoi@estudante.sesisenai.org.br>
2025-09-02 12:46:05 -0300

    Ajuste no banco de dados seguindo as  regras estabelecidas

--- db.sql
+++ db.sql
@@ -5,35 +5,35 @@ USE petshop_db;
 CREATE TABLE clientes(
     id INT AUTO_INCREMENT PRIMARY KEY, 
     nome VARCHAR(120) NOT NULL,
-    cpf VARCHAR(11) NOT NULL,
-    telefone VARCHAR(11) NOT NULL,
-    email VARCHAR(120) NOT NULL
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    telefone VARCHAR(11) NULL,
+    email VARCHAR(120) NULL
 );
 
 CREATE TABLE pets(
     id INT AUTO_INCREMENT PRIMARY KEY, 
     nome VARCHAR(120) NOT NULL,
     cliente_id INT NOT NULL,
-    especie VARCHAR(120) NOT NULL,
+    especie ENUM('Shih Tzu', 'Labrador Retriever', 'Golden Retriever', 'Bulldog Francês', 'Pastor Alemão', 'Poodle', 'Yorkshire Terrier', 'Beagle', 'Chihuahua', 'Husky Siberiano', 'Pug', 'Border Collie', 'Rottweiler', 'Dálmata', 'Lhasa Apso') NOT NULL,
     porte ENUM('Pequeno','Medio','Grande') NOT NULL,
-    nascimento DATE NOT NULL,
+    nascimento DATE NULL,
     CONSTRAINT fk_cliente FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
 
 CREATE TABLE servicos(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(120) NOT NULL,
-    preco DECIMAL(6,2) NOT NULL,
-    duracao_min INT NULL
+    nome VARCHAR(120) NOT NULL UNIQUE,
+    preco DECIMAL(6,2) CHECK(preco >= 0) NOT NULL,
+    duracao_min INT CHECK(duracao_min > 0) NULL
 );
 
 CREATE TABLE agendamentos(
     id INT AUTO_INCREMENT PRIMARY KEY,
     pet_id INT NOT NULL,
+    data_hora DATETIME NOT NULL,
     servico_id INT NOT NULL,
     observacoes VARCHAR(200) NULL,
     status ENUM('Agendado','Concluido','Cancelado') NOT NULL,
-    data_hora DATE NOT NULL,
     
     CONSTRAINT fk_pet FOREIGN KEY (pet_id) REFERENCES pets(id),
     CONSTRAINT fk_servico FOREIGN KEY (servico_id) REFERENCES servicos(id)
